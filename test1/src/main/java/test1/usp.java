115fe4eab917684e1a6716d799c5d929591975d6
Samuil <kotkasmotka@abv.bg>
2021-03-20 01:24:02 +0200

    Team commit

--- test1/src/main/java/test1/usp.java
+++ test1/src/main/java/test1/usp.java
@@ -3,7 +3,13 @@ package test1;
 public class usp {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World");
+		System.out.println("Hello World!!!!!");
+		
+		int nRandomNum=100;
+		
+		String name="Genoslava";
+		
+		System.out.println(name);
 
 	}
 
