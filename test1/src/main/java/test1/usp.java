ac7ec8b673a2ab0015383c1ff937786291645835
Samuil <kotkasmotka@abv.bg>
2021-03-24 00:05:09 +0200

    Last change2

--- test1/src/main/java/test1/usp.java
+++ test1/src/main/java/test1/usp.java
@@ -3,14 +3,15 @@ package test1;
 public class usp {
 
 	public static void main(String[] args) {
-<<<<<<< HEAD
+
 		System.out.println("Hello World");
 		System.out.println("Hello World");
 		int i = 1;
 		int c = 2;
 		int a = 1;
 		
-=======
+		int a2 = 1;
+		
 		System.out.println("Hello World!!!!!");
 		
 		int nRandomNum=10;
@@ -20,9 +21,6 @@ public class usp {
 		System.out.println(name);
 		System.out.println(name);
 		System.out.println(name);
->>>>>>> branch 'master' of https://github.com/genovevaDimitrova/usp.git
-
-		String JojoMojo1 = "\n\n\nJonathan Joestar!";
 		
 		
 		String JojoMojo2 = "\n\n\nJonathan Joestar!!!";
